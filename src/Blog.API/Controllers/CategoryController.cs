dbc1300a51cd48bff26e54a0c9e70d838791c8d9
Luiz Gustavo <luizgustavorosa@outlook.com>
2023-01-09 20:24:39 -0300

    add database

---
+++ src/Blog.API/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version=apiVersion}/[controller]")]
+public class CategoryController : ControllerBase
+{
+
+    [HttpGet("categories")]
+    public IActionResult Get([FromServices]BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+
+        return Ok(categories);
+    }
+}
---
+++ src/Blog.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers;
+
+[Route("")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
